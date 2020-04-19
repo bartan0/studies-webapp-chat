1ea962a95b5bd1b8e2b9d4dde4d55457b24b7523
Bartłomiej Sługocki <0@bartan0.pl>
2020-04-19 19:51:06 +0200

    Added BROKER_URL config var, made broker client module of the server conditional

--- src/services.server/broker/index.js
+++ src/services.server/broker/index.js
@@ -3,25 +3,21 @@ const jwt = require('jsonwebtoken')
 const { Config, WS, WSClient } = App
 
 
-let URL = `http://${Config.BROKER_HOST}`
+if (Config.BROKER_URL) {
+	const token = jwt.sign(
+		{ userId: null },
+		Config.JWT_SECRET
+	)
 
-if (Config.BROKER_PORT)
-	URL += `:${Config.BROKER_PORT}`
+	const client = new WSClient(Config.BROKER_URL, 'broker', { token })
+		.onMessage((type, p) => {
+			if (type === 'event-forwarded')
+				WS.emit(p.resource, p.id, p.type, p.payload, {
+					noCallbacks: true
+				})
+		})
 
-const token = jwt.sign(
-	{ userId: null },
-	Config.JWT_SECRET
-)
+	WS.on('emit', event => client.send('event', event))
 
-
-const client = new WSClient(URL, 'broker', { token })
-	.onMessage((type, p) => {
-		if (type === 'event-forwarded')
-			WS.emit(p.resource, p.id, p.type, p.payload, {
-				noCallbacks: true
-			})
-	})
-
-WS.on('emit', event => client.send('event', event))
-
-module.exports = () => client.connect('broker', null)
+	module.exports = () => client.connect('broker', null)
+}
